a6d4d8f4c29573dfc81e19d90c8a994dd12c65db
jestarray <34615798+jestarray@users.noreply.github.com>
2018-11-08 12:41:05 -0800

    added PlayScene

---
+++ src/scenes/PlayScene.ts
@@ -0,0 +1,12 @@
+import { CST } from "../CST";
+
+export class PlayScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: CST.SCENES.PLAY,
+        });
+    }
+    create(){
+        
+    }
+}
